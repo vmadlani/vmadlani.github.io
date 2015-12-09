5e6e8db2f2676b553f79bec6193635d91edf48dd
vmadlani <vivek.madlani@gmail.com>
2015-12-09 09:47:48 +0000

    project 1..with sounds

--- project1/README.md
+++ project1/README.md
@@ -1,1 +1,75 @@
-# Project_1
+# Project_1: “Drag ’N’ Drop Family Fortunes”
+
+Currently (Dec 2015) I am a student at London General Assembly Web Development Immersive Course.
+The third week assignment was to make a functional game on a browser. 
+
+Inspired by childhood memories as well as with input from my family, I decided to create a game based on one of the longest running game shows of all time (1978), Family Fortunes, otherwise known as Family Feud in America. The game is based on contestants guessing answers to a topic that a survey has been carried out on. Another source of inspiration was something that was mentioned by classmates and my family - teletext games, which were simple and addictive. That’s the look I have decided to go for. My twist on this is called  “Drag ’N’ Drop Family Fortunes”. I hope you enjoy it as much as I do. 
+
+How the game works:
+> The objective of the game is to get as high a score as possible
+> The game stops once a player loses 3 lives
+> Points are scored by dragging answer boxes at the bottom of the page onto the right rows of the table. There are different amounts of points allocated to getting different levels of popularity of answers correct. (40pts for top answer, 30pts for second best, 20pts for third best and 10pts for least popular). 
+
+My approach:
+
+Before I began to write some code, I thought about the different approaches I could use to solve the problem. I also sketched some designs (low fidelity….) and listed out key functionality that would have to be incorporated. 
+
+I decided to design the core of the game around 2 groups of HTML elements, one for the answers, and one for the table that the players need to drag it onto. In order to identify whether an answer has been dragged onto the correct point in the table, I check the text content of the answer box against a ranking of the answers that are stored in an array. A correct answer add points to the relevant player, whereas an incorrect answer reduces their lives. I also created a display board to prompt players and update them of who has won certain rounds / game over. 
+
+A key challenge for me was getting to grips with jQuery UI, in order to be able to drag and drop elements. I had to learn this from scratch and came across some fantastic sources - in partic Tutorials Point, which helped a great deal. I thought the CSS would be easier than it was. I used Google fonts for the first time to get the retro / arcade game font. Lastly, I added in some key sounds for getting a wrong answer / correct answer.
+
+The values of the answers and the topic were sourced from:
+http://www.freequiz4u.com/family%20fortunes%202008.htm#No 1
+which provides topics and the topic 4 answers in each case.
+
+The languages I used were:
+* HTML 5
+* CSS 3 (including animation)
+* JavaScript
+* jQuery (+jQuery UI)
+
+Instructions:
+
+
+Unsolved problems / challenges:
+I used a really long winded way to generate my random choices, (40 lines of code…)
+I wanted to use objects a lot more than I did.
+The most difficult part of the JS code was the reset button, it kept on going wrong an dI needed help from Niall to sort this out. 
+
+If I had another day(s):
+WDI Team console surveys > e.g.. most difficult homework, funniest moments in class so far, favourite pizza toppings etc. 
+Top scores / leaderboard > like in arcade games
+Get more granular survey data, so that points can be scored in line with actual popularity of the answers.
+
+//
+
+CHECKLIST:
+
+### Technical Requirements	
+
+Your app must:	
+* **Render a game in the browser**	DONE
+* **Switch turns** between two players	DONE
+* **Design logic for winning** & **visually display which player won**	DONE
+Reset button	DONE
+* **Include separate HTML / CSS / JavaScript files**	DONE
+* Stick with **KISS (Keep It Simple Stupid)** and **DRY (Don't Repeat Yourself)** principles	DONE
+* Use **Javascript or jQuery** for **DOM manipulation**	DONE
+* **Deploy your game online**, where the rest of the world can access it	DONE
+* Use **semantic markup** for HTML and CSS (adhere to best practices)	DONE
+
+### Necessary Deliverables	
+* A **working game, built by you**, hosted somewhere on the internet	DONE
+* A **link to your hosted working game** in the URL section of your Github repo	DONE
+* A **git repository hosted on Github**, with a link to your hosted game, and frequent commits dating back to the very beginning of the project	DONE
+* **A ``readme.md`` file** with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.	THIS IS IT
+
+### Project Feedback + Evaluation
+* __Project Workflow__: Did you complete the user stories, wireframes, task tracking, and/or ERDs, as specified above? Did you use source control as expected for the phase of the program you’re in (detailed above)?
+* __Technical Requirements__: Did you deliver a project that met all the technical requirements? Given what the class has covered so far, did you build something that was reasonably complex?
+* __Creativity__: Did you added a personal spin or creative element into your project submission? Did you deliver something of value to the end user (not just a login button and an index page)?
+* __Code Quality__: Did you follow code style guidance and best practices covered in class, such as spacing, modularity, and semantic naming? Did you comment your code as your instructors have in class?
+* __Deployment__: Did you deploy your application to a public url using GitHub Pages?
+* __Total__: Your instructors will give you a total score on your project between:
+
+
--- project1/js/gameBoard.js
+++ project1/js/gameBoard.js
@@ -1,15 +1,110 @@
 $(document).ready(function(){
 
-  var content = [
-    ["TOPIC","ANSWER 1","ANSWER 2","ANSWER 3","ANSWER 4"  ],
-    ["NAME A CHARACTER FROM THE WIZARD OF OZ","TIN MAN","DOROTHY","LION","SCARECROW"  ],
-    ["NAME SOMETHING THAT HELPS YOU FALL ASLEEP","ALCOHOL","SLEEPING PILLS","WATCHING TV","LISTEN TO MUSIC"  ],
-    ["NAME SOMETHING THAT DRIVERS DO WITH THEIR HANDS WHEN THEY SHOULD BE AT THE WHEEL","USE A MOBILE PHONE","PICK YOUR NOSE","SMOKE","MESSING WITH RADIO OR CASSETTE"  ],
-    ["NAME SOMETHING ASSOCIATED WITH HIPPIES","DRUGS","LONG HAIR","FLOWER POWER","FREE LOVE"  ],
-    ["NAME A CHOCOLATE BAR BEGINNING WITH THE LETTER 'T'","TWIX","TOBLERONE","TIME-OUT","TOPIC"  ],
-    ["NAME SOMETHING PEOPLE DO WHEN THEY HAVE FINISHED A MEAL","BURP","LIGHT A CIGARETTE","HAVE COFFEE OR TEA","WIPE YOUR MOUTH"  ],
-    ["NAME SOMETHING YOU CAN SEE BUT YOU CANNOT TOUCH","THE SUN","THE MOON","THE CLOUDS","A RAINBOW"  ],
-    ["NAME SOMETHING YOU BUY OR HIRE FOR A WEDDING","WEDDING DRESS","SUIT and TOP HAT AND TAILS","CAR","FLOWERS"  ]
+soundManager.setup({
+  url: 'soundmanager/swf',
+  flashVersion: 9,
+  onready: function() {}
+});
+
+  var myDing = soundManager.createSound({
+   url: 'js/sounds/ding.wav'
+  });
+  
+  var myWrong = soundManager.createSound({
+   url: 'js/sounds/wrong.mp3'
+  });
+
+
+
+var content = [
+  ["Name a famous celebrity who in the last 10 years has made a cameo appearance in Coronation Street","Peter Kay","Prince Charles","Norman Wisdom","Ian McKellen"  ],
+  ["Name any annual ‘Bank Holiday’","Spring Bank","August Bank Holiday","Good Friday","Christmas Day"  ],
+  ["Name any boxing weight division","Heavyweight","Bantam Weight","Lightweight","Flyweight"  ],
+  ["Something you can buy from a hospital shop","Flowers","Newspapers","Chocolate","Cards"  ],
+  ["Name a popular food on a Barbecue?","Sausage","Steak","Chicken","Burgers"  ],
+  ["Name Something Seen On A Front Door","Numbers","Letter Box","Handle","Knockers"  ],
+  ["Give a reason why people don’t use public transport","Never on time","Too crowded","Too expensive","Quicker by car"  ],
+  ["Name something only usable when it hardens","Cement","Ice Cubes","Plaster Cast","Tyres"  ],
+  ["Name something associated with Holland","Windmills","Tulips","Amsterdam","Clogs"  ],
+  ["Name any play by William Shakespeare","Hamlet","Romeo & Juliet","Twelfth Night","Macbeth"  ],
+  ["Something you should do before a long trip","Go to toilet","Fill up with petrol","Take Sweets/food","Secure House"  ],
+  ["Name your sexiest favourite part of the body?","Bottom or Bum","Breasts or Bosom","Eyes","Lips"  ],
+  ["Name A James Bond Theme Tune","Live & Let Die","A View To A Kill","Goldfinger","Nobody Does It Better"  ],
+  ["Name something you would keep secret","Affair","Weight","Bank Balance","Winning Lottery"  ],
+  ["Past or present name any pop group that you can spread on toast","Marmalade","The Jam","Hot Butter","HoneyZ"  ],
+  ["Name something you might win at a fairground","Coconut","Goldfish","Cuddly Toy","Money"  ],
+  ["Name any UK shipping forecast areas","Dogger Bank","Viking","Cromarty","Rockall"  ],
+  ["Something that children take to the beach","Bucket & Spade","Ice Cream","Beach Toys","Footballs/Balls"  ],
+  ["Name a TV Programme that really annoys you?","Big Brother","Trisha","Richard & Judy","Weakest Link"  ],
+  ["Name A children's story featuring a witch","Lion The Witch & The Wardrobe","Snow White","Wizard Of Oz","Hansel & Gretel"  ],
+  ["Name something scrubbed after use","Pans","Bath","Hands","Oven"  ],
+  ["Past or present name a famous real life dog","Lassie","Shep","Digby","Rin Tin Tin"  ],
+  ["Name any UK monarch beginning with the letter ‘E’","Elizabeth","Edward","Ethelred","Edmund"  ],
+  ["Name an excuse a driver might give to the police when caught speeding","Didn’t see the speed sign","Didn’t realise they were","Late for work","Going to Hospital"  ],
+  ["Name any wife of Henry V111","Anne Boleyn","Catherine Howard","Jane Seymour","Catherine Parr"  ],
+  ["Something you do virtually every day of your life","Breath","Sleep","Wake Up","Toilet"  ],
+  ["Name an activity that, at your first attempt could cause a fall?","Skating","Riding a bike","Walking","Drinking"  ],
+  ["Name something that is used only once","Tea Bag","Toilet Paper","Match","Stamp"  ],
+  ["Name a quiet animal","Mouse","Rabbit","Bat","Hamster"  ],
+  ["Name a famous country and western singer","Tammy Wynette","Dolly Parton","Johnny Cash","Willie Nelson"  ],
+  ["Name something a couple might save from their Wedding day","Photos","Cake","Wedding Dress","Ring"  ],
+  ["Name a musical instrument you blow into","Trumpet","Flute","Clarinet","Saxophone"  ],
+  ["Name any London Underground line","Bakerloo","Circle","Victoria","Central"  ],
+  ["Somewhere you would see uniformed guards","Prison","Buckingham Palace","Security Vehicles","Military Bases"  ],
+  ["Name an order a Film Director Might Give","Action","Cut","Lights","Take 1"  ],
+  ["Name a part of the body that moves a lot","Mouth or Tongue","Eye lids","Head","Arms or hands"  ],
+  ["Name something Husbands borrow from wives","Money","Car","Hair Dryer","Deodorant"  ],
+  ["Name a British coin no longer in circulation","Sixpence","Farthing","Threepenny Bit","Half Crown"  ],
+  ["Name something that is sold by the Bunch","Bananas","Flowers","Grapes","Onions"  ],
+  ["Name a reason a husband might send his wife flowers","Anniversary","Valentines’ day","Birthday","Apology"  ],
+  ["Name a city that has hosted the Summer Olympic Games within the last 20 years","Sydney","Athens","Barcelona","Los Angeles"  ],
+  ["Something you associate with Texas","Cowboys","Oil","Dallas","Lone Star"  ],
+  ["Name a breed of strong dog?","Rottweiller","St. Bernard","Bulldog","Huskies"  ],
+  ["Name A method of escaping from prison","Tunnel","Over the wall or fence","Sheets from window","Helicopter"  ],
+  ["Name a type of pole","Flag","May","North or South","Totem"  ],
+  ["Name any Caribbean Island","Jamaica","Barbados","Bahamas","Trinidad"  ],
+  ["Other than shoes or boots, name something you buy from a shoe shop","Polish","Insoles","Laces","Horns"  ],
+  ["Name a job a person might apply for at a supermarket","Checkout person","Self Stacker","Fork lift driver","Manager"  ],
+  ["Name a any UK hit by David Bowie","Life on Mars","Rebel Rebel","Space Oddity","The Laughing Gnome"  ],
+  ["Something you might do with left over Sunday joint meat","Make sandwiches","Fry up","Give to dog","Make soup"  ],
+  ["Name something you can shoot without a firearm","A Film","A Goal","Pool Ball","The Rapids"  ],
+  ["Name something associated with Star Trek","Mr Spock","Enterprise","Captain Kirk","Klingons"  ],
+  ["Name something found in Hawaii","Beaches","Grass Skirts","Palm Trees","Pineapples"  ],
+  ["Other than 007 name any character from a James Bond film","Oddjob","Miss Moneypenny","Jaws","M"  ],
+  ["Name something you would dread to see from an aeroplane window","Engine on fire","Another plane","Broken wings","The pilot"  ],
+  ["Give a reason why a parent might want their child to join the Army","See the World","Earn a living","Discipline","Make them proud"  ],
+  ["Name any item of the 70’s fashion","Tank Top","Hot Pants","Flares","Platform Shoes"  ],
+  ["Something that airlines charge extra for","First Class","Baggage","Taxes","Meals"  ],
+  ["Name something people do in Boots","Hike","Play Sports","Climb","Buy Medicine"  ],
+  ["Name something you get from the post office","Stamps","Pension","TV Licence","Car Tax"  ],
+  ["Name a fish you do not eat","Goldfish","Jellyfish","Starfish","stickleback"  ],
+  ["Name a coloured flag used in Formula One racing","Chequered","Yellow","Black","Red"  ],
+  ["Name something that you might trip over in the family home","Extension Cables","Children’s Toys","Carpet","Shoes"  ],
+  ["Name a reason for sticking out your tongue","Being Rude","Doctors","Having it pierced","Licking your lips"  ],
+  ["Name something that is pulled","Cracker","Leg","A pint","Caravan"  ],
+  ["Something new recruits do at an army training camp","Haircuts","March","PT","Discipline"  ],
+  ["Name the first thing a man does after he finishes making love","Goes to sleep","Smokes a cigarette","Gets dressed and goes home","Kisses and cuddles his partner"  ],
+  ["Past or present name a character who has been a bar maid in the Rovers Return","Betty Turpin/Driver","Bet Lynch","Liz McDonald","Vera Duckworth"  ],
+  ["Name a famous Tom","Jones","Cruise","Hanks","Selleck"  ],
+  ["Name a sweet found in a box of Celebrations","Bounty","Mars Bar","Maltesers","Snicker"  ],
+  ["Name something you might want to have straightened","Teeth","Hair","Nose","Trousers"  ],
+  ["Give a shortened mans name that only has 3 letters","Bob","Tim","Ben","Jim"  ],
+  ["Name a food that does not go well with kissing","Garlic","Onions","Curry","Fish"  ],
+  ["Something a boy scout might have in his back pack","Penknife","Tent","Map","Compass"  ],
+  ["Name Something That Requires Wind To Function","Windmill","Kite","Yacht","Wind chimes"  ],
+  ["Give a slang name for a toilet","Bog","Lavvy","Crapper","Pot"  ],
+  ["Other than your birth certificate, name something on which you would find your date of birth","Passport","Driving Licence","Medical Records","Tombstone"  ],
+  ["TOPIC","ANSWER 1","ANSWER 2","ANSWER 3","ANSWER 4"  ],
+  ["NAME A CHARACTER FROM THE WIZARD OF OZ","TIN MAN","DOROTHY","LION","SCARECROW"  ],
+  ["NAME SOMETHING THAT HELPS YOU FALL ASLEEP","ALCOHOL","SLEEPING PILLS","WATCHING TV","LISTEN TO MUSIC"  ],
+  ["NAME SOMETHING THAT DRIVERS DO WITH THEIR HANDS WHEN THEY SHOULD BE AT THE WHEEL","USE A MOBILE PHONE","PICK YOUR NOSE","SMOKE","MESSING WITH RADIO/CASSETTE"  ],
+  ["NAME SOMETHING ASSOCIATED WITH HIPPIES","DRUGS","LONG HAIR","FLOWERS(POWER)","FREE LOVE"  ],
+  ["NAME A CHOCOLATE BAR BEGINNING WITH THE LETTER 'T'","TWIX","TOBLERONE","TIME-OUT","TOPIC"  ],
+  ["NAME SOMETHING PEOPLE DO WHEN THEY HAVE FINISHED A MEAL","BURP","LIGHT A CIGARETTE","HAVE COFFEE/TEA","WIPE YOUR MOUTH"  ],
+  ["NAME SOMETHING YOU CAN SEE BUT YOU CANNOT TOUCH","THE SUN","THE MOON","THE CLOUDS","A RAINBOW"  ],
+  ["NAME SOMETHING YOU BUY OR HIRE FOR A WEDDING","WEDDING DRESS","SUIT/TOP HAT & TAILS","CAR","FLOWERS"  ],
+  ["NAME A SPORT WHERE BEING TALL IS AN ADVANTAGE","BASKETBALL","GOALKEEPER/SOCCER","HIGH JUMP","NETBALL"  ],
+  ["NAME A NOCTURNAL ANIMAL","HEDGEHOG","OWL","BADGER","CAT"  ]
   ]
 
 var p1Score = 0;
@@ -36,7 +131,7 @@ $("#reset").click(function (){
   //console.log("reset")
 });
 
-var randomIndex = Math.round((Math.random()*7.5)+0.5);
+var randomIndex = Math.round((Math.random()*89));
 console.log(randomIndex);
 var topicAnswerArray = content[randomIndex]; // 0, 1, 2, 3, 4
 $("#topic").text('"' + topicAnswerArray[0] +  '"');
@@ -147,15 +242,17 @@ $( "#droppable1" ).droppable({
   //accept : key1,
   //activeClass: "active",
   hoverClass:  "hover",
-   drop: function(event, ui) {  	
-    if (event.toElement.innerHTML === topicAnswerArray[1])
+   drop: function(event, ui) {  
+      if (event.toElement.innerHTML === topicAnswerArray[1]) 
       {console.log(topicAnswerArray[1]);
+      myDing.play();  
       $( "#droppable1" ).text(topicAnswerArray[1]);
       $("#"+event.toElement.id).text("")
       $( "#droppable1" ).fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
       if (turn%2 === 0) {p1Score=p1Score+40;} else {p2Score=p2Score+40}
       $("#displayBoard").text("Nice one - correct").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);}
     else {console.log(topicAnswerArray[1]);
+      myWrong.play(); 
       $("#displayBoard").text("Sorry dude - that's wrong").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
       if (turn%2 === 0) {p1Lives=p1Lives-1;} else {p2Lives=p2Lives-1};
       console.log(p1Lives);
@@ -180,11 +277,13 @@ $( "#droppable2" ).droppable({
     if (event.toElement.innerHTML === topicAnswerArray[2]) 
       //{console.log(event.toElement.innerHTML);
       {$( "#droppable2" ).text(topicAnswerArray[2]);
+      myDing.play();
       $("#"+event.toElement.id).text("");
       $( "#droppable2" ).fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
       if (turn%2 === 0) {p1Score=p1Score+30;} else {p2Score=p2Score+30};
       $("#displayBoard").text("Nice one - correct").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);}
-    else {$("#displayBoard").text("Sorry dude - that's wrong").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
+    else {myWrong.play(); 
+      $("#displayBoard").text("Sorry dude - that's wrong").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
       if (turn%2 === 0) {p1Lives=p1Lives-1;} else {p2Lives=p2Lives-1};
       console.log(p1Lives);
       console.log(p2Lives);
@@ -210,11 +309,13 @@ $( "#droppable3" ).droppable({
     if (event.toElement.innerHTML === topicAnswerArray[3]) 
       //{$("#"+event.toElement.innerHTML).text("already selected");
       {$( "#droppable3" ).text(topicAnswerArray[3]);
+      myDing.play();
       $("#"+event.toElement.id).text("")
       $( "#droppable3" ).fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
       if (turn%2 === 0) {p1Score=p1Score+20;} else {p2Score=p2Score+20};
     $("#displayBoard").text("Nice one - correct").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);}
-    else {$("#displayBoard").text("Sorry dude - that's wrong").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
+    else {myWrong.play(); 
+      $("#displayBoard").text("Sorry dude - that's wrong").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
       if (turn%2 === 0) {p1Lives=p1Lives-1;} else {p2Lives=p2Lives-1};
       console.log(p1Lives);
       console.log(p2Lives);
@@ -239,12 +340,14 @@ $( "#droppable4" ).droppable({
     if (event.toElement.innerHTML === topicAnswerArray[4])    
       //{$("#"+event.toElement.innerHTML).text("already selected");
       {$( "#droppable4" ).text(topicAnswerArray[4]);
+      myDing.play();
       $("#"+event.toElement.id).text("");
       $( "#droppable4" ).fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
       if (turn%2 === 0) {p1Score=p1Score+10;} else {p2Score=p2Score+10};
       //alert("correct")
       $("#displayBoard").text("Nice one - correct").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);}
-      else {$("#displayBoard").text("Sorry dude - that's wrong").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
+      else {myWrong.play(); 
+        $("#displayBoard").text("Sorry dude - that's wrong").fadeIn(300).fadeOut(300).fadeIn(300).fadeOut(300).fadeIn(300);
       if (turn%2 === 0) {p1Lives=p1Lives-1;} else {p2Lives=p2Lives-1};
       console.log(p1Lives);
       console.log(p2Lives);
@@ -266,7 +369,7 @@ reset = function() {
   turn = 0;
   round++;
   $("#round").text("round: " + (round));
-  var randomIndex = Math.round((Math.random()*7.5)+0.5);
+  var randomIndex = Math.round((Math.random()*89));
   //console.log(randomIndex);
   var topicAnswerArray = content[randomIndex]; // 0, 1, 2, 3, 4
   //$("#C1").text("Hello");
@@ -324,6 +427,9 @@ reset = function() {
   
   };
  
+
+// LEAVE THIS ALONE >> SOUNDMANAGER
+//}); 
 
 
 // LEAVE THESE ALONE  
